c48e37ce3eaa540086c5a5bb3eaabcb300f72066
jsarabia <72255876+javiburn@users.noreply.github.com>
2023-02-07 12:12:30 +0100

    Main removed

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -31,19 +31,3 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str[n] = '\0';
 	return (str);
 }
-/*
-char f(unsigned int i, char c)
-{
-	char str;
-	str = c + 1;
-	return (str);
-}
-
-int main()
-{
-	char str1[] = "abc";
-	char* str2;
-	str2 = ft_strmapi(str1, *f);
-	printf("%s\n", str2);
-}
-*/
